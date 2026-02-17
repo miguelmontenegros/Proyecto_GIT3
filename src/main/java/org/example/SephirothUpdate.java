9db9c8c5701fb14003e03463f9585e8ae96656f8
Sephiroth Leonardo Mota Arismendi <sephirothl.motaa@uqvirtual.edu.co>
2026-02-17 11:59:51 -0500

    Aporte: clase sephirothUpdate

---
+++ src/main/java/org/example/SephirothUpdate.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class SephirothUpdate {
+    public void mostrarMensaje() {
+        System.out.println("Sephiroth actualizo el sistema desde su rama propia.");
+        System.out.println("sincronización Exitosa.");
+    }
+}
